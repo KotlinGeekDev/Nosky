b2a7ab69eb18a0aacb8269e5b50dc54f00efe9b7
KotlinGeek <kotlingeek@protonmail.com>
2022-11-10 17:27:24 +0100

    Comment out navigation-compose code.
    Signed-off-by: KotlinGeek <kotlingeek@protonmail.com>

--- app/src/main/java/kt/nostr/nosky_compose/navigation/AppNavigation.kt
+++ app/src/main/java/kt/nostr/nosky_compose/navigation/AppNavigation.kt
@@ -3,70 +3,54 @@
 package kt.nostr.nosky_compose.navigation
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import kt.nostr.nosky_compose.direct_messages.ui.DiscussionScreen
-import kt.nostr.nosky_compose.direct_messages.ui.Discussions
-import kt.nostr.nosky_compose.home.ui.Home
-import kt.nostr.nosky_compose.notifications.ui.NotificationsScreen
-import kt.nostr.nosky_compose.reusable_ui_components.PostScreen
-import kt.nostr.nosky_compose.reusable_ui_components.ProfileView
-import kt.nostr.nosky_compose.settings.backend.AppThemeState
-import kt.nostr.nosky_compose.settings.ui.SettingsScreen
 
 //TODO: Remove this navigation infrastructure and replace it with Appyx-based navigation structure.
 
-@ExperimentalMaterialApi
-@Composable
-fun AppNavigation(navController: NavHostController,
-                  appThemeState: AppThemeState,
-                  paddingConstraints: PaddingValues = PaddingValues.Absolute(),
-                  onThemeChange: (Boolean) -> Unit){
-    NavHost(navController = navController, startDestination = NavigationItem.Home.route) {
-
-        composable(NavigationItem.Home.route){
-
-            Home(modifier = Modifier.padding(paddingConstraints), navigator = navController)
-        }
-
-//        composable(route = "new_post"){
-//            TestPopupScreen {
-//                navController.navigateUp()
-//            }
+//@ExperimentalMaterialApi
+//@Composable
+//fun AppNavigation(navController: NavHostController,
+//                  appThemeState: AppThemeState,
+//                  paddingConstraints: PaddingValues = PaddingValues.Absolute(),
+//                  onThemeChange: (Boolean) -> Unit){
+//    NavHost(navController = navController, startDestination = NavigationItem.Home.route) {
+//
+//        composable(NavigationItem.Home.route){
+//
+//            Home(modifier = Modifier.padding(paddingConstraints), navigator = navController)
 //        }
-
-        composable(route = "selected_post"){
-            PostScreen { navController.navigateUp() }
-        }
-
-
-        composable(NavigationItem.Notifications.route){
-            NotificationsScreen(navController)
-        }
-
-        composable(NavigationItem.Profile.route){
-            ProfileView(isProfileMine = true, navController = navController, goBack = {})
-        }
-
-        composable(NavigationItem.Messages.route){
-            Discussions(navController)
-        }
-
-        composable(route = "message"){
-            DiscussionScreen(navController)
-        }
-
-        composable(NavigationItem.Settings.route){
-            SettingsScreen(appThemeState, navController = navController, onThemeChange)
-        }
-    }
-}
+//
+////        composable(route = "new_post"){
+////            TestPopupScreen {
+////                navController.navigateUp()
+////            }
+////        }
+//
+//        composable(route = "selected_post"){
+//            PostScreen { navController.navigateUp() }
+//        }
+//
+//
+//        composable(NavigationItem.Notifications.route){
+//            NotificationsScreen(navController)
+//        }
+//
+//        composable(NavigationItem.Profile.route){
+//            ProfileView(isProfileMine = true, navController = navController, goBack = {})
+//        }
+//
+//        composable(NavigationItem.Messages.route){
+//            Discussions(navController)
+//        }
+//
+//        composable(route = "message"){
+//            DiscussionScreen(navController)
+//        }
+//
+//        composable(NavigationItem.Settings.route){
+//            SettingsScreen(appThemeState, navController = navController, onThemeChange)
+//        }
+//    }
+//}
 
 
 
